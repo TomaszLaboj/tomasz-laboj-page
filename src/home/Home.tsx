25b44cde03d978d526c5a317cdc9df343d8df8ef
tomaszlaboj <tomasz.laboj@gmail.com>
2026-06-12 16:20:16 +0100

    add picture

--- src/home/Home.tsx
+++ src/home/Home.tsx
@@ -1,7 +1,15 @@
+import './Home.css';
+
 const Home = () => {
     return (
         <div className="text-paragraph">
             <div>Hello, my name is Tomasz Laboj. I'm a musician and a programmer. The website is divided in two sections Music side and Programming side.</div>
+            <div className="pictures-container">
+                <div className="picture">
+                    <img src="/public/acoustic-guitar-player.png" alt="Person playing on a electric guitar" width="320"/>
+                </div>
+            </div>
+
         </div>
     )
 }
